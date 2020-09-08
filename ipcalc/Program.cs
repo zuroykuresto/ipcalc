9c62a9904257175b1671aa1846f0ffa4c26ce78e
Zuroy Kuresto <zloykpect@gmail.com>
2020-09-08 21:23:34 +0500

    Some change

--- ipcalc/Program.cs
+++ ipcalc/Program.cs
@@ -22,20 +22,20 @@ namespace ipcalc
             }
             char[] delimIpCidr = { '/' };
             int cidr;
-            string[] words = args[0].Split(delimIpCidr);
-            if (words.Length == 2)
+            string[] strParam = args[0].Split(delimIpCidr);
+            if (strParam.Length == 2)
             {
                 
                 Regex ipregx = new Regex(@"^(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])$");
-                MatchCollection result = ipregx.Matches(words[0]);
+                MatchCollection result = ipregx.Matches(strParam[0]);
                 if (result.Count > 0)
                 {
                     char[] delimIp = { '.' };
-                    string[] ipS = words[0].Split(delimIp);
+                    string[] ipS = strParam[0].Split(delimIp);
                     InternetProtocolAddress ip = new InternetProtocolAddress(byte.Parse(ipS[0]), byte.Parse(ipS[1]), byte.Parse(ipS[2]), byte.Parse(ipS[3]));
-                    if (Int32.TryParse(words[1], out cidr))
+                    if (Int32.TryParse(strParam[1], out cidr))
                     {
-                        cidr = Convert.ToInt32(words[1]);
+                        cidr = Convert.ToInt32(strParam[1]);
                         if (cidr < 32)
                         {
                             IPCalculation ipc = new IPCalculation(ip, byte.Parse(Convert.ToString(cidr)));
